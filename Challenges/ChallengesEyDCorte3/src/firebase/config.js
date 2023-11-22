1dcefac4c843b60cb3776354e639dc2b86db2f67
david-ale <david_ale.sanchez@uao.edu.co>
2023-11-22 00:37:29 -0500

    chall 14 primera pt

---
+++ Challenges/ChallengesEyDCorte3/src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAsqCRLTdcorAMskvRqOVC2tZusVDTxEio",
+  authDomain: "fir-bc09b.firebaseapp.com",
+  projectId: "fir-bc09b",
+  storageBucket: "fir-bc09b.appspot.com",
+  messagingSenderId: "1089067722148",	
+  appId: "1:1089067722148:web:0e709c1a60208e02fccd31",
+  measurementId: "G-YCQ0SDZLGH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export {app,auth}
